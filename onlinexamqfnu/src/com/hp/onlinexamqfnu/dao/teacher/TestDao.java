8ab64d3f3c24b58dedf8d53275adb0aea91da7bf
Administrator <Administrator@PC-67>
2018-01-23 21:06:09 +0800

    题目增加、查询、修改功能上线

---
+++ onlinexamqfnu/src/com/hp/onlinexamqfnu/dao/teacher/TestDao.java
@@ -0,0 +1,45 @@
+package com.hp.onlinexamqfnu.dao.teacher;
+
+import java.util.List;
+import java.util.Map;
+
+import com.hp.onlinexamqfnu.po.Test;
+
+public class TestDao implements ITestDao{
+
+	@Override
+	public void createTest(Test t) {
+		String sql = "insert into test values()";
+		try {
+			
+		} catch (Exception e) {
+			// TODO: handle exception
+		}
+	}
+
+	@Override
+	public List<Map<String, Object>> findTestsByTeaId(int teaId) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public Map<String, Object> findTestById(int id, int teaId) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public Map<String, Object> findStudentTestsById(int studentid, int testid) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public List<Map<String, Object>> getTestByStudent(int id, String currData) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+	
+
+}
